d2ca9ec95da836abc9516c56a371f195209da909
Victor Hugo <88410254+victorh21@users.noreply.github.com>
2022-05-17 12:49:56 -0300

    Update EXERCÍCIO4.js

--- EXERCÍCIO4.js
+++ EXERCÍCIO4.js
@@ -3,4 +3,4 @@
 let dias = [20]
 
  let horas = [8]
-console.log('seu salario por mes é R$'+salarios+', seu salario por dia é R$'+salarios/dias+', seu salario por hora é R$'+dias/horas);
+console.log('seu salario por mes é R$'+salarios+', seu salario por dia é R$'+salarios/dias+', seu salario por hora é R$'+salrios/dias/horas);
